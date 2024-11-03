09c797d7b01e389f120a56a96304c32db4de8f2a
Sanchez <sanchezbarry@gmail.com>
2024-11-03 08:17:32 +0800

    removed quotation errors for deployment

--- my-project/src/components/Hello.tsx
+++ my-project/src/components/Hello.tsx
@@ -17,14 +17,14 @@ export default function Hello() {
             alt={"logo"}
             className="mx-auto flex-shrink-0 rounded-full shadow-2xl pb-2"
         />
-        Hi, I'm
+        Hi, I&apos;m
         <Image
             src="/Sanchez_Logo_white-Full.svg"
             width={170}
             height={170}
             alt={"logo"}
             className="inline flex-shrink-0 rounded-md shadow-2xl"
-        />. I'm a 
+        />. I&apos;m a 
         <br />
         <FlipWords className="mx-auto" words={words} /> 
         </div>
@@ -34,10 +34,10 @@ export default function Hello() {
     </div>
           <div className="max-w-4xl text-md mx-auto font-normal text-neutral-600 dark:text-neutral-400 justify-center items-center px-4">
           <p>
-          A marketer turned software engineer, after discovering my love for coding after dabbling with a few websites. I'm excited to use my experience as a marketer to build apps that are intuitive and feel like second nature to use.
+          A marketer turned software engineer, after discovering my love for coding after dabbling with a few websites. I&apos;m excited to use my experience as a marketer to build apps that are intuitive and feel like second nature to use.
           <br />
           <br />
-          Here some things I've created, and the technologies I'm familiar with.
+          Here some things I&apos;ve created, and the technologies I&apos;m familiar with.
           </p>
       </div>
       </>
