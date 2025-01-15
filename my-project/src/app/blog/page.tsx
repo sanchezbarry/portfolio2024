ecad5b583ec6c536798f47eaf7494e343ff983f5
Sanchez <sanchezbarry@gmail.com>
2025-01-15 15:36:17 +0800

    me page updates

--- my-project/src/app/blog/page.tsx
+++ my-project/src/app/blog/page.tsx
@@ -11,6 +11,9 @@ export default function Blog() {
   <section className="mx-auto w-11/12 md:w-1/2 pt-20 mt-20 flex flex-col gap-16 mb-20">
     <header className="font-cormorantGaramond font-light text-6xl  text-center">
       <h1>dev notes</h1>
+      <p className="text-lg pt-5">Welcome to dev notes! dev notes are scribblings of what I&apos;ve learnt as a software developer.
+        I plan to write down my understanding of everything - from the simple stuff like arrays to complex algorithms and data structures.
+      </p>
     </header>
     <section className="md:grid md:grid-cols-2 flex flex-col gap-10">
       {articles !== null && 
--- my-project/src/app/me/page.tsx
+++ my-project/src/app/me/page.tsx
@@ -3,7 +3,27 @@ import Hello from "@/components/Hello"
 import ComingSoon from "@/components/ComingSoon"
 
 export default function Me() {
-  return <>
-  <ComingSoon />
-  </>;
+  return (
+    <>
+      <div className="mt-32 pb-10 flex justify-center items-center px-4">
+        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          Me.
+        </div>
+      </div>
+      <div className="flex justify-center items-center md:px-40 px-10">
+        <p className="text-lg">This space is planned for personal projects, and other things I&apos;m working. Stay tuned!
+          In the meantime, here&apos;s a PayNow QR code to suport my work.
+        </p>
+      </div>
+      <div className="flex justify-center items-center pt-10 px-10">
+        <Image
+        src="/paynow.jpg"
+        width={500}
+        height={500}
+        alt="PayNow QR code"
+        className="rounded-lg"
+        />
+      </div>
+    </>
+  )
 }
