49e315b1548ce0d4c2cdf0b097bdb1da8620a95d
Sanchez <sanchezbarry@gmail.com>
2025-06-11 21:03:33 +0800

    added new project

--- my-project/src/components/ProjectsGrid.tsx
+++ my-project/src/components/ProjectsGrid.tsx
@@ -41,7 +41,7 @@ const CoffeeContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/coffee">
+      <Link href="https://github.com/sanchezbarry/coffee" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -51,7 +51,7 @@ const CoffeeContent = () => {
 
 
 
-        <Link href="https://coffee-cu87.onrender.com/">
+        <Link href="https://coffee-cu87.onrender.com/" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           Website
@@ -84,7 +84,7 @@ const AlcoholContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/sanchezbarry">
+      <Link href="https://github.com/sanchezbarry/sanchezbarry" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -94,7 +94,7 @@ const AlcoholContent = () => {
 
 
 
-        <Link href="https://sanchezbarry.github.io/sanchezbarry/#">
+        <Link href="https://sanchezbarry.github.io/sanchezbarry/#" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           Website
@@ -127,7 +127,7 @@ const SoftwareContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/Software-Engineer-Job-Portal-Frontend">
+      <Link href="https://github.com/sanchezbarry/Software-Engineer-Job-Portal-Frontend" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -135,7 +135,7 @@ const SoftwareContent = () => {
         </button>
         </Link>
 
-        <Link href="https://lunfy.github.io/Software-Engineer-Job-Portal-Frontend/">
+        <Link href="https://lunfy.github.io/Software-Engineer-Job-Portal-Frontend/" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           Website
@@ -171,7 +171,7 @@ const WorkoutContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/Workouter-App">
+      <Link href="https://github.com/sanchezbarry/Workouter-App" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -205,7 +205,7 @@ const ChurchContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/cck">
+      <Link href="https://github.com/sanchezbarry/cck" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -215,7 +215,7 @@ const ChurchContent = () => {
 
 
 
-        <Link href="https://cck.org.sg/sermons.html">
+        <Link href="https://cck.org.sg/sermons.html" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           Website
@@ -251,7 +251,7 @@ const SanchezOneContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/portfolio">
+      <Link href="https://github.com/sanchezbarry/portfolio" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -292,7 +292,7 @@ const SanchezTwoContent = () => {
             </p>
 
       </div>
-      <Link href="https://github.com/sanchezbarry/portfolio2024">
+      <Link href="https://github.com/sanchezbarry/portfolio2024" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           GitHub
@@ -300,7 +300,51 @@ const SanchezTwoContent = () => {
         </button>
         </Link>
 
-        <Link href="https://sanchezbarry.com">
+        <Link href="https://sanchezbarry.com" target="_blank" rel="noopener noreferrer">
+        <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+          
+          Website
+          
+        </button>
+        </Link>
+
+    </>
+  );
+};
+
+const FPL = () => {
+  return (
+    <>
+      <div
+
+        className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+          >
+            <Image
+              src="/FPL.png"
+              alt="This Portfolio Website Preview"
+              height="500" 
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+            />
+            <p className="pt-5 text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+              I wanted to create a website to give ma breakdown of my fantasy premier league.
+              </span>{" "}
+              The main technology behind this project is making API calls to the Fantasy Premier League API, and displaying the data in a user-friendly way.
+              Players can enter which gameweek they want to view, and the website will display the players in their team, their points, and what transfers their opponents have made.
+              This project is still a work in progress, and I plan to add more features in the future, such as an input field so anyone can enter their league id and view their league data.
+            </p>
+
+      </div>
+      <Link href="https://github.com/sanchezbarry/fplstats" target="_blank" rel="noopener noreferrer">
+        <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+          
+          GitHub
+          
+        </button>
+        </Link>
+
+        <Link href="https://fplstats-e611.vercel.app/" target="_blank" rel="noopener noreferrer">
         <button className="mr-3 shadow-[inset_0_0_0_2px_#616467] text-black px-10 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
           
           Website
@@ -313,6 +357,13 @@ const SanchezTwoContent = () => {
 };
 
 const data = [
+    {
+    category: "an FPL League data breakdown.",
+    tech: "NextJS | React | TailwindCSS | Shadcn | TypeScript | API",
+    title: "FPL League Viewer",
+    src: "/football.jpg",
+    content: <FPL />,
+  },
   {
     category: "A website to show off my skills and projects.",
     tech: "NextJS | React | TailwindCSS | Aceternity UI | TypeScript | CRUD",
